17fe22ecac26c4dd6e1299a2846794d2f590de4b
Naomi Ezaki <naomi.ezaki28@gmail.com>
2019-02-10 21:18:30 +0800

    modified files

---
+++ GUI-Tkinter/MarineAnimalImageClassifierGUI.py
@@ -0,0 +1,25 @@
+# Import libraries
+import tkinter as tk
+from trial import perform
+
+# Create Instance
+windows = tk.Tk()
+
+# Add a title to GUI
+windows.title("Marine Animal Image Classifier")
+
+#create frame
+coolFrame = tk.Frame(windows)
+coolFrame.pack()
+
+# Put "False" to make non-resizable
+windows.resizable(False, False)
+
+#tk.[widgetname](root window, properties/cofiguration)
+# def perform():
+#     tk.Label(windows, text="This is our first label").pack()
+
+perform(tk, windows)
+
+# Start the GUI/Create the main loop
+windows.mainloop()
---
+++ GUI-Tkinter/trial.py
@@ -0,0 +1,2 @@
+def perform(tk, windows):
+    tk.Label(windows, text="This is our first label").pack()
