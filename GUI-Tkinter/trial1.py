291298e9e2210e34c0e2f731177a3bf6d5b57ef5
Ellaine Gabotero <gabotero.ellaine@gmail.com>
2019-02-15 11:17:11 +0800

    Added display result GUI

--- GUI-Tkinter/trial1.py
+++ GUI-Tkinter/trial1.py
@@ -13,18 +13,24 @@ canvas.pack()
 
 #
 label = tk.Label(root, text="Choose image to classify")
-label.place(anchor='n', relx=0.5, rely=0.3)
+label.place(anchor='n', relx=0.5, rely=0.1)
 
 button = tk.Button(root, text="Import")
-button.place(anchor='n', relx=0.5, rely=0.4)
+button.place(anchor='n', relx=0.5, rely=0.2)
 
 # 
 label = tk.Label(root, text="Begin the process")
-label.place(anchor='s', relx=0.5, rely=0.6)
+label.place(anchor='s', relx=0.5, rely=0.4)
 
 button = tk.Button(root, text="Start")
-button.place(anchor='s', relx=0.5, rely=0.7)
+button.place(anchor='s', relx=0.5, rely=0.5)
 
+# Output
+frame = tk.Frame(root, bd=10)
+frame.place(relx=0.5, rely=0.6, relwidth=0.90, relheight=0.4, anchor='n')
+
+label = tk.Label(frame, bg='white')
+label.place(relwidth=1, relheight=1)
 
 root.resizable(False, False)
 root.mainloop()
